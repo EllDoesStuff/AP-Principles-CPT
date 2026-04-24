588e73d46d7baf7ad7bac53d0d97b5db6a2361b0
MandelSettDoesStuff <pylarinos20297451@student.nhvweb.net>
2026-04-24 18:24:53 +0000

    yummu Co-authored-by: slash7xx <slash7xx@users.noreply.github.com>

--- GRUMBIN.py
+++ GRUMBIN.py
@@ -17,25 +17,77 @@ if Atk + Def + Mag >= 10 or Atk + Def + Mag <= 10:
     Def = int(input("Input a number for Defense:"))
     Mag = int(input("Input a number for Magic:"))
 
-print(f"So Grumblin has {Atk} attack, {Def} defense, and {Mag} magic. Now, he can fight for his treasure!")
+print(f"So Grumblin has {Atk} attack, {Def} defense, and {Mag} magic. Now, he can fight for his treasure! There's five rounds until Flim-bim, good luck!")
 Hp = 10
-card1 = Atk * 2
-card2 = Atk + turn
-card3 = Mag * 2
-card4 = Mag + turn
-card5 = Def * 2
-card6 = Def + turn
-Mults = [card1, card2, card3, card4, card5, card6]
-card7 = Hp * 2
-card8 = Hp + turn
-Heals = [card7, card8] 
+
+
+
+
 
 turn = 1
 
-while act != True:
-    turn + 1
 
 
-def Damage():
+   
+def fights():
+while sumt<5: #NEED TO FIGURE OUT HOW TO MAKE 5 NPC FiGHTS APPEAR BEFORE FLIMBIM
+    attacks = ["Scratch","Bite","Rock Throw"]
+    strength = []
+    durability = []
+    turn = []
+    npc_hp = []
+    creature = ["Wolf", "Mutant spider", "Cyclops","Leviathan", "Yeti", "Gorgon", "Ghoul", "Minotaur"]
+    classes = ["Attacker","Defender","Speedster","All-Rounder", "Technical"]
+    cre = (random.choice(creature))
+    cla = (random.choice(classes))
+    att = (random.choice(attacks))
+    if classes == "Attacker":
+        npc_hp = [10]
+        strength = [3]
+    if classes == "Defender":
+        npc_hp = [15]
+        strength = [2]
+    if classes == "Speedster":
+        npc_hp = [9]
+        strength = [1]
+    if classes == "All-Rounder":
+        npc_hp = [13]
+        strength = [2]
+    if classes == "Technical":
+        npc_hp = [11]
+        strength = [2]
+    print(f"A {cre} approaches, it's a {cla} type. It may hit you with {attacks}. It has {npc_hp} hp.")
+    if turn % 2 == 0:
+        Grumfight = input("type Attack to attack with Goblin fist, and Magic to throw a fireball!")
+        if Grumfight == "Attack":
+            npc_hp - Atk
+            print(f"Goblin fist! Enemy now has {npc_hp}!")
+            time.sleep(0.5)
+            turn = turn + 1
+        elif Grumfight == "Magic":
+            npc_hp - Mag
+            print(f"Fireball! Enemy now has {npc_hp}!")
+            time.sleep(0.5)
+            turn = turn + 1
+    elif turn % 2 == 1:
+        
+        
+    card1 = Atk * 2
+    card2 = Atk + turn
+    card3 = Mag * 2
+    card4 = Mag + turn
+    card5 = Def * 2
+    card6 = Def + turn
+    card7 = Hp * 2
+    card8 = Hp + turn
+    Cards = [card1, card2, card3, card4, card5, card6, card7, card8]
+    random.choice(Cards)
 
-start()
+#Flimbim activation
+boss_hp = 50
+boss_strength = 5
+boss_attacks = ["Ground Pound", "Scratch Barrage", "Magic beam"]
+print(f"Flim-bim appears! It's the boss and it can hit you with {boss_attacks}. It has {boss_hp} hp.")
+
+
+
